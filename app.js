859f9f35010e4cf19a624d59d9ba13c23fb40aca
camoros <caitlin.welsh9@gmail.com>
2018-01-02 22:15:29 -0700

    delete first child

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
 function onReady() {
-  let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
-
-  function createNewToDo () {
-    toDos.push({
-      title: newToDoText.value,
-      complete: false
+  const deleteToDo = document.getElementById('deleteToDo');
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
-    createNewToDo();
+
+    // get the text
+    let title = newToDoText.value;
+
+    // create a new li
+    let newLi = document.createElement('li');
+
+    //create a new input
+    let checkbox = document.createElement('input');
+
+    //set the input's type to checkbox
+    checkbox.type = "checkbox";
+
+
+    //set the title
+    newLi.textContent = title;
+
+    //attach the checkbox to the li
+    newLi.appendChild(checkbox);
+
+    //attach the li to the ul
+    toDoList.appendChild(newLi);
+
+    //empty the input
     newToDoText.value = '';
-  })
-    });
-  }
+  });
+
+  deleteToDo.addEventListener('click', event => {
+    event.preventDefault();
+
+    let children = document.getElementById('toDoList');
+    toDoList.children[0].remove();
+  });
 }
 
 window.onload = function() {
